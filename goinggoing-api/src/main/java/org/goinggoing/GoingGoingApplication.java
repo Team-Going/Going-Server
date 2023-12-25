6377ed047a9f82643a3c50ede50a3ca84ceadcf8
sunwoong <sunwoong1122@gmail.com>
2023-12-25 23:02:10 +0900

    chore: common 모듈 의존성 추가 (#1)

--- goinggoing-api/src/main/java/org/goinggoing/GoingGoingApplication.java
+++ goinggoing-api/src/main/java/org/goinggoing/GoingGoingApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GoingGoingApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GoingGoingApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GoingGoingApplication.class, args);
+    }
 }
