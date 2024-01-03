b85d5589fbfdafd959bf0233ad7e9ce85faf5db6
sunwoong <sunwoong1122@gmail.com>
2024-01-03 15:18:36 +0900

    feat: jwt dto 클래스 구현 (#3)

---
+++ doorip-api/src/main/java/org/doorip/auth/jwt/Token.java
@@ -0,0 +1,17 @@
+package org.doorip.auth.jwt;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record Token(
+        String accessToken,
+        String refreshToken
+) {
+    public static Token of(String accessToken, String refreshToken) {
+        return Token.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+}
