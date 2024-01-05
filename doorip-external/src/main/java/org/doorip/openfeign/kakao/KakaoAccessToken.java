98d8a91ed7a702c4a73090d26683b96afe86090b
gardening-y <60008786@naver.com>
2024-01-05 11:46:14 +0900

    feat: 카카오 access token response dto 구현 (#9)

---
+++ doorip-external/src/main/java/org/doorip/openfeign/kakao/KakaoAccessToken.java
@@ -0,0 +1,20 @@
+package org.doorip.openfeign.kakao;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class KakaoAccessToken {
+    private static final String TOKEN_TYPE = "Bearer ";
+    private String accessToken;
+
+    public static KakaoAccessToken createKakaoAccessToken(String accessToken) {
+        return new KakaoAccessToken(accessToken);
+    }
+
+    public String getAccessTokenWithTokenType() {
+        return TOKEN_TYPE + accessToken;
+    }
+}
---
+++ doorip-external/src/main/java/org/doorip/openfeign/kakao/KakaoAccessTokenInfo.java
@@ -0,0 +1,10 @@
+package org.doorip.openfeign.kakao;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public record KakaoAccessTokenInfo(
+        Long id
+) {
+}
