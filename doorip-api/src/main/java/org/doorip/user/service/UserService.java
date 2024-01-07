9ec98d8269e8bcfba0c2c7bad89e0991c7764ab4
gardening-y <60008786@naver.com>
2024-01-07 21:27:19 +0900

    feat: 회원가입 service 구현 (#16)

--- doorip-api/src/main/java/org/doorip/user/service/UserService.java
+++ doorip-api/src/main/java/org/doorip/user/service/UserService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.doorip.auth.jwt.JwtProvider;
 import org.doorip.auth.jwt.Token;
 import org.doorip.exception.EntityNotFoundException;
+import org.doorip.exception.UnauthorizedException;
 import org.doorip.message.ErrorMessage;
 import org.doorip.openfeign.apple.AppleOAuthProvider;
 import org.doorip.openfeign.kakao.KakaoOAuthProvider;
@@ -11,6 +12,7 @@ import org.doorip.user.domain.Platform;
 import org.doorip.user.domain.RefreshToken;
 import org.doorip.user.domain.User;
 import org.doorip.user.dto.request.UserSignInRequest;
+import org.doorip.user.dto.request.UserSignUpRequest;
 import org.doorip.user.dto.response.UserResponse;
 import org.doorip.user.repository.RefreshTokenRepository;
 import org.doorip.user.repository.UserRepository;
@@ -19,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.doorip.user.domain.Platform.APPLE;
 import static org.doorip.user.domain.Platform.getEnumPlatformFromStringPlatform;
+import static org.doorip.user.domain.User.createUser;
 
 @RequiredArgsConstructor
 @Service
@@ -41,6 +44,17 @@ public class UserService {
         return UserResponse.of(issueToken);
     }
 
+    @Transactional(noRollbackFor = UnauthorizedException.class)
+    public UserResponse signUp(String token, UserSignUpRequest request) {
+        Platform enumPlatform = getEnumPlatformFromStringPlatform(request.platform());
+        String platformId = getPlatformId(token, enumPlatform);
+        User savedUser = saveUser(request, platformId, enumPlatform);
+        Token issueToken = jwtProvider.issueToken(savedUser.getId());
+        updateRefreshToken(issueToken.refreshToken(), savedUser);
+
+        return UserResponse.of(issueToken);
+    }
+
     private String getPlatformId(String token, Platform platform) {
         if (platform == APPLE) {
             return appleOAuthProvider.getApplePlatformId(token);
@@ -56,5 +70,10 @@ public class UserService {
     private void updateRefreshToken(String refreshToken, User user) {
         user.updateRefreshToken(refreshToken);
         refreshTokenRepository.save(RefreshToken.createRefreshToken(user.getId(), refreshToken));
+    }
+
+    private User saveUser(UserSignUpRequest request, String platformId, Platform enumPlatform) {
+        User user = createUser(request.name(), request.intro(), platformId, enumPlatform);
+        return userRepository.save(user);
     }
 }
