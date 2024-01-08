d03c4d0d0257002f4d5dafb915417a5f306baff4
gardening-y <60008786@naver.com>
2024-01-09 02:19:36 +0900

    feat: 여행 생성 service 구현 (#27)

---
+++ doorip-api/src/main/java/org/doorip/trip/service/TripService.java
@@ -0,0 +1,82 @@
+package org.doorip.trip.service;
+
+import lombok.RequiredArgsConstructor;
+import org.doorip.exception.EntityNotFoundException;
+import org.doorip.exception.InvalidValueException;
+import org.doorip.message.ErrorMessage;
+import org.doorip.trip.domain.Participant;
+import org.doorip.trip.domain.Role;
+import org.doorip.trip.domain.Trip;
+import org.doorip.trip.dto.request.TripCreateRequest;
+import org.doorip.trip.dto.response.TripCreateResponse;
+import org.doorip.trip.repository.ParticipantRepository;
+import org.doorip.trip.repository.TripRepository;
+import org.doorip.user.domain.User;
+import org.doorip.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import static org.doorip.trip.domain.Participant.createParticipant;
+import static org.doorip.trip.domain.Trip.createTrip;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class TripService {
+    private final TripRepository tripRepository;
+    private final ParticipantRepository participantRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public TripCreateResponse createTripAndParticipant(Long userId, TripCreateRequest request) {
+        User findUser = getUser(userId);
+        validateDate(request);
+        String code = createCode(6);
+        Trip createdTrip = saveTrip(request, code);
+        saveParticipant(request.styleA(), request.styleB(), request.styleC(), request.styleD(), request.styleE(), findUser, createdTrip);
+
+        return TripCreateResponse.of(createdTrip);
+    }
+
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorMessage.USER_NOT_FOUND));
+    }
+
+    private void validateDate(TripCreateRequest request) {
+        if (request.endDate().isBefore(LocalDate.now()) || request.endDate().isBefore(request.startDate())) {
+            throw new InvalidValueException(ErrorMessage.INVALID_DATE_TYPE);
+        }
+    }
+
+    private String createCode(int num) {
+        String code;
+
+        do {
+            String uuid = UUID.randomUUID().toString();
+            System.out.println(uuid);
+            code = uuid.substring(0, num);
+
+        } while (validateDuplicateCode(code));
+
+        return code;
+    }
+
+    private boolean validateDuplicateCode(String code) {
+        return tripRepository.existsByCode(code);
+    }
+
+    private Trip saveTrip(TripCreateRequest request, String code) {
+        Trip trip = createTrip(request.title(), request.startDate(), request.endDate(), code);
+        return tripRepository.save(trip);
+    }
+
+    private void saveParticipant(int styleA, int styleB, int styleC, int styleD, int styleE,
+                                        User user, Trip trip) {
+        Participant participant = createParticipant(Role.HOST, styleA, styleB, styleC, styleD, styleE, user, trip);
+        participantRepository.save(participant);
+    }
+}
