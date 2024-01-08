d0d3df755fb8b50e4d235ede5dff2926dd928146
gardening-y <60008786@naver.com>
2024-01-09 05:45:12 +0900

    feat: 회원가입 중복 검증 로직 추가 (#28)

--- doorip-domain/src/main/java/org/doorip/user/repository/UserRepository.java
+++ doorip-domain/src/main/java/org/doorip/user/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByPlatformAndPlatformId(Platform platform, String platformId);
+
+    boolean existsUserByPlatformAndPlatformId(Platform platform, String platformId);
 }
