f244123a519820ee0d85a1c175092ee2a1faf328
sunwoong <sunwoong1122@gmail.com>
2024-01-09 23:16:49 +0900

    del: 불필요 dto 속성 제거 (#38)

--- doorip-api/src/main/java/org/doorip/trip/dto/request/TodoCreateRequest.java
+++ doorip-api/src/main/java/org/doorip/trip/dto/request/TodoCreateRequest.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record TodoCreateRequest(
-        Long tripId,
         String title,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
         LocalDate endDate,
