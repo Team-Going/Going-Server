a10562399a246ec5b9e2bbddd7f35f06a13310f7
sunwoong <sunwoong1122@gmail.com>
2024-01-10 06:21:40 +0900

    feat: 여행 TODO 조회 요청 파라미터 상수화 (#34)

--- doorip-api/src/main/java/org/doorip/common/Constants.java
+++ doorip-api/src/main/java/org/doorip/common/Constants.java
@@ -6,4 +6,6 @@ public abstract class Constants {
     public static final String CHARACTER_TYPE = "utf-8";
     public static final String INCOMPLETE = "incomplete";
     public static final String COMPLETE = "complete";
+    public static final String OUR = "our";
+    public static final String MY = "my";
 }
