6e17e0b7868415d0666c3dc0cf8b6c247a2b74b5
sunwoong <sunwoong1122@gmail.com>
2024-01-10 06:25:10 +0900

    feat: 여행 Our & My TODO 전체 조회 비즈니스 로직 구현 (#34)

--- doorip-api/src/main/java/org/doorip/trip/service/TodoService.java
+++ doorip-api/src/main/java/org/doorip/trip/service/TodoService.java
@@ -1,38 +1,47 @@
 package org.doorip.trip.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.doorip.common.Constants;
 import org.doorip.exception.EntityNotFoundException;
 import org.doorip.exception.InvalidValueException;
 import org.doorip.message.ErrorMessage;
-import org.doorip.trip.domain.Participant;
-import org.doorip.trip.domain.Secret;
-import org.doorip.trip.domain.Todo;
-import org.doorip.trip.domain.Trip;
+import org.doorip.trip.domain.*;
 import org.doorip.trip.dto.request.TodoCreateRequest;
+import org.doorip.trip.dto.response.TodoAllocatorResponse;
+import org.doorip.trip.dto.response.TodoGetResponse;
 import org.doorip.trip.repository.ParticipantRepository;
 import org.doorip.trip.repository.TodoRepository;
 import org.doorip.trip.repository.TripRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.doorip.trip.domain.Allocator.createAllocator;
 
+@Slf4j
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 @Service
 public class TodoService {
     private final TripRepository tripRepository;
     private final ParticipantRepository participantRepository;
     private final TodoRepository todoRepository;
 
+    @Transactional
     public void createTripTodo(Long tripId, TodoCreateRequest request) {
         validateAllocators(request.allocators());
         Trip findTrip = getTrip(tripId);
         Todo todo = createTodo(request, findTrip);
         createAllocators(request.allocators(), todo);
         todoRepository.save(todo);
+    }
+
+    public List<TodoGetResponse> getTripTodos(Long userId, Long tripId, String category, String progress) {
+        List<Todo> todos = getTripTodosAccordingToCategoryAndProgress(userId, tripId, category, progress);
+        return getTripTodosResponse(userId, todos);
     }
 
     private void validateAllocators(List<Long> allocators) {
@@ -57,8 +66,67 @@ public class TodoService {
         });
     }
 
+    private List<Todo> getTripTodosAccordingToCategoryAndProgress(Long userId, Long tripId, String category, String progress) {
+        if (category.equals(Constants.OUR)) {
+            return getOurTodosAccordingToProgress(tripId, progress);
+        } else if (category.equals(Constants.MY)) {
+            return getMyTodosAccordingToProgress(userId, tripId, progress);
+        }
+        throw new InvalidValueException(ErrorMessage.INVALID_REQUEST_PARAMETER_VALUE);
+    }
+
+    private List<TodoGetResponse> getTripTodosResponse(Long userId, List<Todo> todos) {
+        List<TodoGetResponse> response = new ArrayList<>();
+        todos.forEach(todo -> {
+            List<Allocator> allocators = todo.getAllocators();
+            List<TodoAllocatorResponse> allocatorResponses = getAllocatorResponses(userId, allocators);
+            TodoAllocatorResponse ownerAllocator = getOwnerAllocator(allocatorResponses);
+            sortAllocators(ownerAllocator, allocatorResponses);
+            response.add(TodoGetResponse.of(todo, allocatorResponses));
+        });
+        return response;
+    }
+
     private Participant getParticipant(Long participantId) {
         return participantRepository.findById(participantId)
                 .orElseThrow(() -> new EntityNotFoundException(ErrorMessage.PARTICIPANT_NOT_FOUND));
+    }
+
+    private List<Todo> getOurTodosAccordingToProgress(Long tripId, String progress) {
+        if (progress.equals(Constants.INCOMPLETE)) {
+            return todoRepository.findOurTodoByTripId(tripId, Secret.OUR, Progress.INCOMPLETE);
+        } else if (progress.equals(Constants.COMPLETE)) {
+            return todoRepository.findOurTodoByTripId(tripId, Secret.OUR, Progress.COMPLETE);
+        }
+        throw new InvalidValueException(ErrorMessage.INVALID_REQUEST_PARAMETER_VALUE);
+    }
+
+    private List<Todo> getMyTodosAccordingToProgress(Long userId, Long tripId, String progress) {
+        if (progress.equals(Constants.INCOMPLETE)) {
+            return todoRepository.findMyTodoByTripId(tripId, userId, Progress.INCOMPLETE);
+        } else if (progress.equals(Constants.COMPLETE)) {
+            return todoRepository.findMyTodoByTripId(tripId, userId, Progress.COMPLETE);
+        }
+        throw new InvalidValueException(ErrorMessage.INVALID_REQUEST_PARAMETER_VALUE);
+    }
+
+    private List<TodoAllocatorResponse> getAllocatorResponses(Long userId, List<Allocator> allocators) {
+        return new ArrayList<>(allocators.stream()
+                .map(allocator -> TodoAllocatorResponse.of(userId, allocator))
+                .toList());
+    }
+
+    private TodoAllocatorResponse getOwnerAllocator(List<TodoAllocatorResponse> allocatorResponses) {
+        return allocatorResponses.stream()
+                .filter(TodoAllocatorResponse::isOwner)
+                .findFirst()
+                .orElse(null);
+    }
+
+    private void sortAllocators(TodoAllocatorResponse ownerAllocator, List<TodoAllocatorResponse> allocatorResponses) {
+        if (ownerAllocator != null) {
+            allocatorResponses.remove(ownerAllocator);
+            allocatorResponses.add(0, ownerAllocator);
+        }
     }
 }
