a8f5865aa1a5873ad7989d713b0fff7038c6493d
sunwoong <sunwoong1122@gmail.com>
2024-01-09 23:17:49 +0900

    refactor: 여행 TODO 생성 API 스펙 변경 (#38)

--- doorip-api/src/main/java/org/doorip/trip/api/TodoApiController.java
+++ doorip-api/src/main/java/org/doorip/trip/api/TodoApiController.java
@@ -8,19 +8,21 @@ import org.doorip.trip.dto.request.TodoCreateRequest;
 import org.doorip.trip.service.TodoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
-@RequestMapping("/api/todos")
+@RequestMapping("/api/trips")
 @Controller
 public class TodoApiController {
     private final TodoService todoService;
 
-    @PostMapping
-    public ResponseEntity<ApiResponse<?>> createTripTodo(@RequestBody final TodoCreateRequest request) {
-        todoService.createTripTodo(request);
+    @PostMapping("/{tripId}/todos")
+    public ResponseEntity<ApiResponse<?>> createTripTodo(@PathVariable final Long tripId,
+                                                         @RequestBody final TodoCreateRequest request) {
+        todoService.createTripTodo(tripId, request);
         return ApiResponseUtil.success(SuccessMessage.CREATED);
     }
 }
