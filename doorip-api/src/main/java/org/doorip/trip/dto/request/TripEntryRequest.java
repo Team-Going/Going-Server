bea05eb9d4f8a26746d26aa6eb654ab4c9ae64ce
gardening-y <60008786@naver.com>
2024-01-11 00:02:14 +0900

    feat: 여행 입장 request dto 구현 (#42)

---
+++ doorip-api/src/main/java/org/doorip/trip/dto/request/TripEntryRequest.java
@@ -0,0 +1,10 @@
+package org.doorip.trip.dto.request;
+
+public record TripEntryRequest(
+        int styleA,
+        int styleB,
+        int styleC,
+        int styleD,
+        int styleE
+) {
+}
