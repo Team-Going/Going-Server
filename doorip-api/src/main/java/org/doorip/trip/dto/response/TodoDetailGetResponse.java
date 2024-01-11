5a58da2974fbe2ab80a6ceac56e0443663e87c42
sunwoong <sunwoong1122@gmail.com>
2024-01-12 05:18:07 +0900

    feat: 여행 TODO 상세 조회 응답 dto 클래스 구현 (#78)

---
+++ doorip-api/src/main/java/org/doorip/trip/dto/response/TodoDetailGetResponse.java
@@ -0,0 +1,30 @@
+package org.doorip.trip.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.doorip.trip.domain.Secret;
+import org.doorip.trip.domain.Todo;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record TodoDetailGetResponse(
+        String title,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+        LocalDate endDate,
+        List<TodoAllocatorResponse> allocators,
+        String memo,
+        boolean secret
+) {
+    public static TodoDetailGetResponse of(Todo todo, List<TodoAllocatorResponse> allocators) {
+        return TodoDetailGetResponse.builder()
+                .title(todo.getTitle())
+                .endDate(todo.getEndDate())
+                .allocators(allocators)
+                .memo(todo.getMemo())
+                .secret(Secret.of(todo.getSecret()))
+                .build();
+    }
+}
