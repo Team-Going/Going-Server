4be49e1b3faf4c94329529181c66874db860429b
sunwoong <sunwoong1122@gmail.com>
2024-01-12 02:54:17 +0900

    feat: 여행 Our TODO 상세 조회 참여자 응답 dto 클래스 구현 (#54)

---
+++ doorip-api/src/main/java/org/doorip/trip/dto/response/TripParticipantResponse.java
@@ -0,0 +1,22 @@
+package org.doorip.trip.dto.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import org.doorip.trip.domain.Participant;
+import org.doorip.user.domain.User;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record TripParticipantResponse(
+        Long participantId,
+        String name,
+        int result
+) {
+    public static TripParticipantResponse of(Participant participant) {
+        User user = participant.getUser();
+        return TripParticipantResponse.builder()
+                .participantId(participant.getId())
+                .name(user.getName())
+                .result(user.getResult().getNumResult())
+                .build();
+    }
+}
