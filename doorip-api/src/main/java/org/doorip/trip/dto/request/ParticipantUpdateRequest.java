80e90c357b8ee9d6ebbb4b2a48de49167113b01a
gardening-y <60008786@naver.com>
2024-03-03 03:27:06 +0900

    feat: 여행 성향 테스트 수정 request dto 구현 (#131)

---
+++ doorip-api/src/main/java/org/doorip/trip/dto/request/ParticipantUpdateRequest.java
@@ -0,0 +1,10 @@
+package org.doorip.trip.dto.request;
+
+public record ParticipantUpdateRequest(
+        int styleA,
+        int styleB,
+        int styleC,
+        int styleD,
+        int styleE
+) {
+}
