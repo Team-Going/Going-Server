465881193e43465c65ebf802d0ee9a4438675307
sunwoong <sunwoong1122@gmail.com>
2024-03-07 17:16:16 +0900

    fix: 여행 친구 전체 조회 dto 수정 (#146)

--- doorip-api/src/main/java/org/doorip/trip/dto/response/TripParticipantGetResponse.java
+++ doorip-api/src/main/java/org/doorip/trip/dto/response/TripParticipantGetResponse.java
@@ -8,15 +8,19 @@ import java.util.List;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record TripParticipantGetResponse(
+        List<String> bestPrefer,
         List<TripParticipantResponse> participants,
         List<TripStyleResponse> styles
 ) {
-    public static TripParticipantGetResponse of(List<Participant> participants, List<TripStyleResponse> styles) {
+    public static TripParticipantGetResponse of(List<String> bestPrefer, List<Participant> participants, List<List<Integer>> styles) {
         return TripParticipantGetResponse.builder()
+                .bestPrefer(bestPrefer)
                 .participants(participants.stream()
                         .map(TripParticipantResponse::of)
                         .toList())
-                .styles(styles)
+                .styles(styles.stream()
+                        .map(TripStyleResponse::from)
+                        .toList())
                 .build();
     }
 }
