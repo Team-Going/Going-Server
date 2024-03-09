2ae613a5b99f021aace5de1053ab8964d5e559db
sunwoong <sunwoong1122@gmail.com>
2024-03-09 18:17:08 +0900

    fix: 여행 친구 전체 조회 응답 dto 수정 (#151)

--- doorip-api/src/main/java/org/doorip/trip/dto/response/TripParticipantGetResponse.java
+++ doorip-api/src/main/java/org/doorip/trip/dto/response/TripParticipantGetResponse.java
@@ -2,9 +2,12 @@ package org.doorip.trip.dto.response;
 
 import lombok.AccessLevel;
 import lombok.Builder;
+import org.doorip.common.Constants;
 import org.doorip.trip.domain.Participant;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Builder(access = AccessLevel.PRIVATE)
 public record TripParticipantGetResponse(
@@ -12,15 +15,16 @@ public record TripParticipantGetResponse(
         List<TripParticipantResponse> participants,
         List<TripStyleResponse> styles
 ) {
-    public static TripParticipantGetResponse of(List<String> bestPrefer, List<Participant> participants, List<List<Integer>> styles) {
+    public static TripParticipantGetResponse of(List<String> bestPrefer, List<Participant> participants, List<List<Integer>> rates, List<List<Integer>> counts) {
+        List<TripStyleResponse> styles = new ArrayList<>();
+        IntStream.range(Constants.START_STYLE_POS, Constants.END_STYLE_POS)
+                .forEach(i -> styles.add(TripStyleResponse.of(rates.get(i), counts.get(i))));
         return TripParticipantGetResponse.builder()
                 .bestPrefer(bestPrefer)
                 .participants(participants.stream()
                         .map(TripParticipantResponse::of)
                         .toList())
-                .styles(styles.stream()
-                        .map(TripStyleResponse::from)
-                        .toList())
+                .styles(styles)
                 .build();
     }
 }
