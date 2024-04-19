fba7b13b9a2f12f5d85dbafeaaafe896397fa50c
sunwoong <sunwoong1122@gmail.com>
2024-04-19 20:16:40 +0900

    chore: async 설정 (#160)

---
+++ doorip-event-subscriber/src/main/java/org/doorip/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package org.doorip.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+    @Bean
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(30);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-executor-");
+        executor.initialize();
+        return executor;
+    }
+}
